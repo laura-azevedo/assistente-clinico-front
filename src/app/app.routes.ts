c5e0f380155dae9108dbd405997a964cc82595df
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-06-03 19:10:16 -0300

    chore(routes): update routes to include cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
+import { Cadastro } from './auth/cadastro/cadastro';
 
 export const routes: Routes = [
-  { path: '', component: Login }
+  { path: '', component: Login },
+  { path: 'cadastro', component: Cadastro}
 ];
