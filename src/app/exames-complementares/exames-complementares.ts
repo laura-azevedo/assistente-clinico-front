ee588ba5ae8cdc168ec3d047d6bac1ab7fb24a36
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-06-05 15:14:49 -0300

    feat(navbar): implement navbar and add it to the other pages

--- src/app/exames-complementares/exames-complementares.ts
+++ src/app/exames-complementares/exames-complementares.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../shared/components/header/header';
+import { Navbar } from '../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-exames-complementares',
-  imports: [],
+  imports: [Navbar, Header],
   templateUrl: './exames-complementares.html',
   styleUrl: './exames-complementares.css'
 })
--- src/app/exames/exames.ts
+++ src/app/exames/exames.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../shared/components/navbar/navbar';
+import { Header } from '../shared/components/header/header';
 
 @Component({
   selector: 'app-exames',
-  imports: [],
+  imports: [Navbar, Header],
   templateUrl: './exames.html',
   styleUrl: './exames.css'
 })
