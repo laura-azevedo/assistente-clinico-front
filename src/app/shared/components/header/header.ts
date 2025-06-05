301892428199e1daee9b8b06ad27d92bdd5c4d4f
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-06-05 14:58:11 -0300

    feat(header): implement header and add logout button and hamburger menu toggle

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BotaoSair } from '../botao-sair/botao-sair';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [BotaoSair],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
