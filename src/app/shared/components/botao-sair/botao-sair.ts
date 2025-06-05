4995c9c1bd893933bca6c20cf27ceb06b9ac0376
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-06-05 14:56:23 -0300

    feat(botao-sair): implement logout button

--- src/app/shared/components/botao-sair/botao-sair.ts
+++ src/app/shared/components/botao-sair/botao-sair.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-botao-sair',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class BotaoSair {
 
+  constructor(private router: Router) {}
+
+  logout(): void {
+    this.router.navigate(['/'])
+  }
 }
