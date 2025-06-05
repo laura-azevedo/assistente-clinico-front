9d1cc7a56d871164bf2be70c9c3c7cc38dd92d0e
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-06-05 10:55:04 -0300

    chore(routes): update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { Cadastro } from './auth/cadastro/cadastro';
+import { Entrevista } from './entrevista/entrevista';
+import { Exames } from './exames/exames';
+import { ExamesComplementares } from './exames-complementares/exames-complementares';
+import { CasoConfig } from './caso-config/caso-config';
 
 export const routes: Routes = [
   { path: '', component: Login },
-  { path: 'cadastro', component: Cadastro}
+  { path: 'cadastro', component: Cadastro },
+  { path: 'entrevista', component: Entrevista },
+  { path: 'exames', component: Exames},
+  { path: 'exames-complementares', component: ExamesComplementares},
+  { path: 'configuracao-caso', component: CasoConfig }
 ];
