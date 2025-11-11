e98dbd4ae2da371c0418f5263d8bf1d551a01e15
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-11-11 20:16:14 -0300

    feat(home): add startAppointment function

--- src/app/shared/components/home/home.ts
+++ src/app/shared/components/home/home.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Header } from '../header/header';
 import { Navbar } from '../navbar/navbar';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { AppointmentStateService } from '../../services/appointment.service';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +16,7 @@ import { Navbar } from '../navbar/navbar';
 export class Home {
   @ViewChild(Navbar) navbar!: Navbar;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient, private appointmentState: AppointmentStateService) {}
 
   ngOnInit() {}
 
@@ -21,7 +24,19 @@ export class Home {
     this.navbar.toggle();
   }
 
-  goToEntrevista() {
+  async startAppointment() {
+  try {
+    const token = localStorage.getItem('token');
+    console.log('O TOKEN É: ', localStorage.getItem('token'));
+    const res: any = await this.http.post(
+      `${environment.apiUrl}/entrevista/start`,
+      {},
+      { headers: { Authorization: `Bearer ${token}` } }
+    ).toPromise();
+
+    this.appointmentState.appointmentId = res.appointment_id;
     this.router.navigate(['/entrevista']);
+  } catch (err) {
+    console.error('Erro ao iniciar atendimento', err);}
   }
 }
