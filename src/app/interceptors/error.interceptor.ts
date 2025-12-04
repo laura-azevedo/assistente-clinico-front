c61c864b7d4b87472194fa456a9811360ee7c071
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-12-04 10:22:17 -0300

    fix(error.interceptor): edit 401 error message and auto redirect to login page

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -5,9 +5,11 @@ import {
 } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toastr = inject(ToastrService);
+  const router = inject(Router);
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
@@ -16,7 +18,9 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
       if (error.status === 0) {
         errorMsg = 'Não foi possível conectar ao servidor. Entre em contato com o administrador.';
       } else if (error.status === 401) {
-        errorMsg = 'Você não está autorizado. Faça login novamente.';
+        errorMsg = 'Seu token expirou. Faça login novamente.';
+        localStorage.removeItem('token');
+        router.navigate(['/login'])
       } else if (error.status === 404) {
         errorMsg = 'Recurso não encontrado.';
       } else if (error.status === 500) {
