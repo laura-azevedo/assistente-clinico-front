9559fdf82d72cd5762b3ffad67422b5f5194c606
Laura <azevedolaura02@gmail.com>
2026-05-07 16:43:39 -0300

    fix: fix environment path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://assistente-clinico-node-yskc.onrender.com/'
+  apiUrl: 'https://assistente-clinico-node-yskc.onrender.com'
   // apiUrl: 'http://127.0.0.1:3000'
     // apiUrl: 'http://127.0.0.1:8080'
 };
